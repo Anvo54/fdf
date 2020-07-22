5ba887a4f012edd8c247a5ac5034a5489cc387cb
Antti Vornanen <anttivornanen@Antti-iMac.local>
2020-07-22 20:43:48 +0300

    made separate function to format the initial data

--- main.c
+++ main.c
@@ -12,6 +12,24 @@
 
 #include "fdf.h"
 
+void fromat_data(mlx_data_t *data, map_t *kartta)
+{
+	data->width = 800;
+	data->height = 600;
+	if ((data->mlx_ptr = mlx_init()) == (void *)0)
+		exit(0);
+	if ((data->mlx_win = mlx_new_window(data->mlx_ptr, data->width , data->height, "FDF Hive")) == NULL)
+		exit(0);
+	data->project = cartesian;
+	data->map = kartta;
+	data->img_ptr = mlx_new_image(data->mlx_ptr, data->width, data->height);
+	data->img_data = mlx_get_data_addr(data->img_ptr, &(data->bpp), &(data->size_line), &(data->endian));
+	data->zoom = 1;
+	data->translate_x = 0;
+	data->translate_y = 0;
+	data->z_height = 0;
+}
+
 int main(int argc, char **argv)
 {
 	mlx_data_t data;
@@ -19,20 +37,7 @@ int main(int argc, char **argv)
 	int fd;
 	fd = open(argv[1], O_RDONLY);
 	kartta = create_map(fd);
-	data.width = 800;
-	data.height = 600;
-	if ((data.mlx_ptr = mlx_init()) == (void *)0)
-		exit(0);
-	if ((data.mlx_win = mlx_new_window(data.mlx_ptr, data.width , data.height, "FDF Hive")) == NULL)
-		return (-1);
-	data.project = cartesian;
-	data.map = kartta;
-	data.img_ptr = mlx_new_image(data.mlx_ptr, data.width, data.height);
-	data.img_data = mlx_get_data_addr(data.img_ptr, &(data.bpp), &(data.size_line), &(data.endian));
-	data.zoom = 1;
-	data.translate_x = 0;
-	data.translate_y = 0;
-	data.z_height = 0;
+	fromat_data(&data, kartta);
 	close(fd);
 	mlx_key_hook(data.mlx_win, deal_key, &data);
 	print_map(data.map, data);
